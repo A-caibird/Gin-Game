cd9a5234d8235db4e3bdcc723ed67d17657d0f65
Acaibird <durbannhawkey42@gmail.com>
2024-05-09 16:24:04 +0800

    fix:Cross-domain requests are not allowed to carry the request header Content-Type.add gin abort. fixes #16

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,13 +10,13 @@ func Cors(c *gin.Context) {
 	// Configure CORS response headers
 	c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
 	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-	c.Header("Access-Control-Allow-Headers", "Content-Type")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, User-Agent")
 	c.Header("Access-Control-Allow-Credentials", "true")
 	// handle preflight request
 	if c.Request.Method == "OPTIONS" {
 		c.Header("Access-Control-Allow-Max-Age", "86400")
 		c.Writer.WriteHeader(http.StatusOK)
-		return
+		c.Abort()
 	}
 	c.Next()
 }
