3c731a2ef681bce8dd70d0a857b14014b3346122
Acaibird <durbannhawkey42@gmail.com>
2024-05-11 21:15:39 +0800

    feat: add log system, write in file and console

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"Game/handler"
 	"Game/middleware"
+	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 )
 
 func main() {
 	r := gin.New()
+	// log
+	logFilePath := "./log/gin.log"
+	file, err := os.Create(logFilePath)
+	if err != nil {
+		color.Red("%#v", err)
+	}
+	defer file.Close()
+	// writer
+	multiWriter := io.MultiWriter(file, os.Stdout)
 	// Register a global middleware
-	r.Use(gin.Logger())
+	r.Use(gin.LoggerWithWriter(multiWriter))
 	r.Use(gin.Recovery())
 	r.Use(middleware.Cors)
 	r.Use(middleware.Auth)
@@ -20,5 +33,6 @@ func main() {
 	}, func(context *gin.Context) {
 		context.String(200, "%s", "fasfasfdas")
 	})
+	r.POST("/login", handler.Login)
 	r.Run(":8000")
 }
