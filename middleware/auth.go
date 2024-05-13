ed49e0583f85d241848d220c02ad9ea5e7aeae63
Acaibird <durbannhawkey42@gmail.com>
2024-05-13 16:10:03 +0800

    fix:anth middleware Requests that have not been processed will still be processed next. fixes #20

--- middleware/auth.go
+++ middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	session "Game/sessions"
+	"Game/tools"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -10,16 +11,21 @@ import (
 
 func Auth(c *gin.Context) {
 	if c.Request.URL.Path == "/login" {
+		tools.Info(c.Request.URL.Path)
 		c.Next()
 	}
+
 	store := session.NewSessionStore()
 	sessions, err := store.Get(c.Request, "session")
 	if err != nil {
 		fmt.Fprint(os.Stderr, err.Error())
 		c.String(404, "%s", err.Error())
+		c.Abort()
+		return
 	}
 	if sessions.IsNew {
 		c.Writer.WriteHeader(http.StatusUnauthorized)
+		c.Abort()
 		return
 	}
 	username := c.Query("username")
@@ -27,9 +33,10 @@ func Auth(c *gin.Context) {
 		if s, e := val.(string); e {
 			if s == username {
 				c.Next()
+			} else {
+				c.AbortWithStatus(http.StatusUnauthorized)
 			}
 		}
 	}
-	c.Writer.WriteHeader(http.StatusUnauthorized)
-	c.Abort()
+	c.AbortWithStatus(http.StatusUnauthorized)
 }
