09ee026107fe33295820bdffd5e74c617253a535
Acaibird <durbannhawkey42@gmail.com>
2024-06-07 12:59:29 +0800

    fix:no default avatar

--- handler/get_avatar.go
+++ handler/get_avatar.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"Game/tools"
 	"github.com/gin-gonic/gin"
+	"os"
 	"regexp"
 )
 
@@ -10,8 +11,11 @@ import (
 func GetAvatar(c *gin.Context) {
 	id := c.Param("id")
 	path := tools.Conf.RootPath.Path + "/public/avatar/" + id + ".png"
-	//
-	c.File(path)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		c.File(tools.Conf.RootPath.Path + "/public/avatar/" + "default.png")
+	} else {
+		c.File(path)
+	}
 	// download file or preview file
 	url := c.Request.URL.Path
 	patter := "download"
