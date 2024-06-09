04ffc03d6d3fc68e75792d5cb9adaa3e581b77d9
Acaibird <durbannhawkey42@gmail.com>
2024-06-09 22:59:11 +0800

    feat:modify logic of search user handler

---
+++ handler/get_name.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"Game/mysql"
+	"Game/mysql/entiy"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+func GetNameById(c *gin.Context) {
+	//
+	id := c.Param("id")
+	Id, _ := strconv.Atoi(id)
+	//
+	db, err := mysql.NewOrmDb()
+	if err != nil {
+		c.AbortWithStatus(500)
+		return
+	}
+	//
+	var user entiy.User
+	db.Model(entiy.User{
+		ID: uint(Id),
+	}).First(&user)
+	c.JSON(200, user.Name)
+}
